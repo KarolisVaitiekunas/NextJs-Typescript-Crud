7390f17fc40ff2fef32d2791498c7d6ec741a4ef
KarolisVaitiekunas <70333362+KarolisVaitiekunas@users.noreply.github.com>
2021-09-17 18:26:06 +0300

    Added proper error validation messages.

--- server/src/utils/yup/authSchemas.ts
+++ server/src/utils/yup/authSchemas.ts
@@ -5,9 +5,18 @@ import * as yup from "yup";
  */
 const registerSchema = yup.object({
   body: yup.object({
-    username: yup.string().min(4).max(32).required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(4).max(255).required(),
+    email: yup.string().email("Please enter a valid email.").required("Email field is required."),
+    username: yup
+      .string()
+      .matches(/^([a-zA-Z0-9 _-]+)$/, "Username cannot have special characters in it.")
+      .min(4, "Username field must be at least 4 characters long.")
+      .max(32, "Username cannot be longer than 32 characters")
+      .required("Username field is required."),
+    password: yup
+      .string()
+      .min(4, "Password must have at least 4 characters.")
+      .max(255, "Password cannot have more than 255 characters")
+      .required(),
   }),
 });
 
@@ -16,21 +25,31 @@ const registerSchema = yup.object({
  */
 const loginSchema = yup.object({
   body: yup.object({
+    email: yup.string().email("Please enter a valid email.").required("Email field is required."),
+    password: yup
+      .string()
+      .min(4, "Password must have at least 4 characters.")
+      .max(255, "Password cannot have more than 255 characters")
+      .required(),
+  }),
+});
+
+/**
+ * Validation schema for forgotpassword body
+ */
+const forgotpasswordSchema = yup.object({
+  body: yup.object({
     email: yup.string().email().required(),
+  }),
+});
+
+/**
+ * Validation schema for resetpassword body
+ */
+const resetpasswordSchema = yup.object({
+  body: yup.object({
     password: yup.string().min(4).max(255).required(),
   }),
 });
 
-// const forgotpasswordSchema = yup.object({
-//   body: yup.object({
-//     email: yup.string().email().required(),
-//   }),
-// });
-
-// const resetpasswordSchema = yup.object({
-//   body: yup.object({
-//     password: yup.string().min(4).max(255).required(),
-//   }),
-// });
-
-export { registerSchema, loginSchema };
+export { registerSchema, loginSchema, forgotpasswordSchema, resetpasswordSchema };
--- server/src/utils/yup/postSchemas.ts
+++ server/src/utils/yup/postSchemas.ts
@@ -2,8 +2,16 @@ import * as yup from "yup";
 
 const createPostSchema = yup.object({
   body: yup.object({
-    title: yup.string().min(4).max(26).required(),
-    body: yup.string().min(15).max(700).required(),
+    title: yup
+      .string()
+      .min(4, "Title field must be at least 4 characters long.")
+      .max(26, "Title field cannot be longer than 26 characters")
+      .required(),
+    body: yup
+      .string()
+      .min(15, "Body field must be at least 15 characters long.")
+      .max(700, "Body field cannot be longer than 700 characters")
+      .required(),
   }),
 });
 
